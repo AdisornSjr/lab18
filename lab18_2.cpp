9797bf3e9ca103ace4f8e4b87a0eba2486a4016e
fluke <adisorn.sjr@gmail.com>
2023-02-14 16:40:18 +0700

    com

--- lab18_2.cpp
+++ lab18_2.cpp
@@ -1,19 +1,20 @@
 #include<iostream>
 using namespace std;
 
-________ myString(_____________________){
-	_______ = new ____________;    
-	for(int i = 0; i < N;i++) ____________ = 'A'+i;
-	__________ = 0;
+void myString(char *&z,int N){
+    z = new char[N];   
+    for(int i = 0; i < N; i++) z[i] = 'A'+i;
+    N = 0;
+
 }
 
 int main(){
-	char *p;
-	int N;	
-	cout << "N = ";
-	cin >> N;		
-	myString(p,N);
-	cout << p;	
-	delete [] p;
-	return 0;
+    char *p;
+    int N;
+    cout << "N = ";
+    cin >> N;
+    myString(p,N);
+    cout << p;
+    delete [] p;
+    return 0;
 }
